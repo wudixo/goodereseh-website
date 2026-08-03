855eb352182758d0ed92720c7f4645d889fb94ec
wudixo <wudixo@yahoo.com>
2026-08-03 12:15:02 +0100

    next

--- js/app.js
+++ js/app.js
@@ -6,38 +6,4 @@ if (form) {
 
         event.preventDefault();
 
-        const formData = new FormData(form);
-
-        try {
-
-            const response = await fetch(form.action, {
-
-                method: "POST",
-
-                body: formData,
-
-                headers: {
-                    Accept: "application/json"
-                }
-
-            });
-
-            if (response.ok) {
-
-                window.location.href = "thank-you.html";
-
-            } else {
-
-                alert("Something went wrong. Please try again.");
-
-            }
-
-        } catch (error) {
-
-            alert("Unable to send your enquiry. Please check your internet connection.");
-
-        }
-
-    });
-
-}
+        const
