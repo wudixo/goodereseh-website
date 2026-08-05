e7441a5fe7dd7ecc97ae1c86109f45aa67c36222
wudixo <wudixo@yahoo.com>
2026-08-05 23:47:37 +0100

    Fix Stripe product IDs and update artwork catalogue

--- server/productCatalog.js
+++ server/productCatalog.js
@@ -18,6 +18,139 @@ module.exports = {
         artwork: "Hope",
         type: "Original Artwork",
         price: 30000
+    },
+
+
+    fragments_original: {
+        artwork: "Fragments of Thought",
+        type: "Original Abstract Charcoal Artwork",
+        price: 400
+    },
+
+
+    fragments_print_A4: {
+        artwork: "Fragments of Thought Fine Art Print A4",
+        type: "Limited Edition Fine Art Print",
+        price: 50
+    },
+
+
+    fragments_print_A3: {
+        artwork: "Fragments of Thought Fine Art Print A3",
+        type: "Limited Edition Fine Art Print",
+        price: 95
+    },
+
+
+    fragments_print_A2: {
+        artwork: "Fragments of Thought Fine Art Print A2",
+        type: "Limited Edition Fine Art Print",
+        price: 175
+    },
+
+
+    fragments_print_A1: {
+        artwork: "Fragments of Thought Fine Art Print A1",
+        type: "Limited Edition Fine Art Print",
+        price: 300
+    },
+
+
+    reflections_original: {
+        artwork: "Reflections",
+        type: "Original Hyperrealistic Eye Drawing",
+        price: 3000
+    },
+
+
+    reflections_print_A4: {
+        artwork: "Reflections Fine Art Print A4",
+        type: "Limited Edition Fine Art Print",
+        price: 85
+    },
+
+
+    reflections_print_A3: {
+        artwork: "Reflections Fine Art Print A3",
+        type: "Limited Edition Fine Art Print",
+        price: 150
+    },
+
+
+    reflections_print_A2: {
+        artwork: "Reflections Fine Art Print A2",
+        type: "Limited Edition Fine Art Print",
+        price: 275
+    },
+
+
+    reflections_print_A1: {
+        artwork: "Reflections Fine Art Print A1",
+        type: "Limited Edition Fine Art Print",
+        price: 500
+    },
+
+
+    fela_original: {
+        artwork: "Fela Kuti",
+        type: "Original Hand Drawn Portrait",
+        price: 4500
+    },
+
+
+    fela_print_A4: {
+        artwork: "Fela Kuti Fine Art Print A4",
+        type: "Limited Edition Fine Art Print",
+        price: 95
+    },
+
+
+    fela_print_A3: {
+        artwork: "Fela Kuti Fine Art Print A3",
+        type: "Limited Edition Fine Art Print",
+        price: 175
+    },
+
+
+    fela_print_A2: {
+        artwork: "Fela Kuti Fine Art Print A2",
+        type: "Limited Edition Fine Art Print",
+        price: 325
+    },
+
+
+    fela_print_A1: {
+        artwork: "Fela Kuti Fine Art Print A1",
+        type: "Limited Edition Fine Art Print",
+        price: 550
+    },
+
+
+    the_stance_print_A4: {
+        artwork: "The Stance Fine Art Print A4",
+        type: "Limited Edition Fine Art Print",
+        price: 95
+    },
+
+
+    the_stance_print_A3: {
+        artwork: "The Stance Fine Art Print A3",
+        type: "Limited Edition Fine Art Print",
+        price: 175
+    },
+
+
+    the_stance_print_A2: {
+        artwork: "The Stance Fine Art Print A2",
+        type: "Limited Edition Fine Art Print",
+        price: 325
+    },
+
+
+    the_stance_print_A1: {
+        artwork: "The Stance Fine Art Print A1",
+        type: "Limited Edition Fine Art Print",
+        price: 550
     }
 
 };
